550a9a5f51d0ba99b43e28b2be09d064dfbae6c3
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-09 23:46:50 +0530

    adding files

---
+++ task-1/src/pages/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./navbar.css";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/posts">HOME</Link>
+        </li>
+        <li>
+          <Link to="/products">ABOUT</Link>
+        </li>
+        <li>
+          <Link to="/users">OTHERS</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ task-1/src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div>
+      <p>hohohohohooooh</p>
+    </div>
+  )
+}
+
+export default Products
